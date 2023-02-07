7a81343a13aba91e3cd0bd517520d33a5e07f7d4
Harsh Desai <harshedsai18502@gmail.com>
2023-02-07 18:25:54 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// //For sliding to particular div
-// const HomeBtn = document.getElementById("#HomeBtn");
-// const CourseBtn = document.getElementById("#CourseBtn");
-// const ImageBtn = document.getElementById("#ImageBtn");
-// const ContactBtn = document.getElementById("#ContactBtn");
-// const AboutBtn = document.getElementById("#AboutBtn");
-
-
-
-// const Home = document.getElementById("#Home");
-// const Course = document.getElementById("#Course");
-// const Image = document.getElementById("#Image");
-// const Contact = document.getElementById("#Contact");
-// const About = document.getElementById("#About");
-
-// HomeBtn.addEventListener("click", () => {
-//     console.log('clicked');
-//   Home.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-// });
-
-// CourseBtn.addEventListener("click", () => {
-//   Course.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-// });
-
-// ImageBtn.addEventListener("click", () => {
-//   Image.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-// });
-
-// ContactBtn.addEventListener("click", () => {
-//   Contact.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-// });
-
-// AboutBtn.addEventListener("click", () => {
-//   About.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-// });
 
 //For image Slider
 const slides = document.querySelectorAll(".slide");
